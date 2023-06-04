d48297659df8e6b05d5672b08a2477d5892ad768
BETTINA K PETER <129045305+heavenlybee@users.noreply.github.com>
2023-06-04 20:46:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import './App.css';
 import Home from './components/pages/Home';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './components/pages/Dashboard';
 import Services from './components/pages/Services';
 import LogOut from './components/pages/LogOut';
@@ -10,23 +10,20 @@ import AboutUs from './components/pages/AboutUs';
 import ContactUs from './components/pages/ContactUs';
 import Certificates from './components/pages/Certificates';
 
-
 function App() {
   return (
-    <>
-      <Router>
-        <Navbar/>
-        <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/dashboard' component={Dashboard} />
-          <Route path='/services' component={Services} />
-          <Route path='/log-out' component={LogOut} />
-          <Route path='/about-us'component={AboutUs} />  
-          <Route path='/contact' component={ContactUs}/>   
-          <Route path='/certificates' component={Certificates}/>
-        </Switch>
-        </Router>
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/log-out" element={<LogOut />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/certificates" element={<Certificates />} />
+      </Routes>
+    </Router>
   );
 }
 
